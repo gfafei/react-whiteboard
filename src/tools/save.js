cffe4dc51c238e05e2c7dd46e9f0894cd101a978
afei <afei_gf@foxmail.com>
2020-10-26 10:31:05 +0800

    save

--- src/tools/save.js
+++ src/tools/save.js
@@ -7,6 +7,24 @@ class Save extends Tool {
     this.icon = 'icon-save';
     this.label = 'Save';
   }
+
+  handleClick() {
+    const state = this.state;
+    const u = navigator.userAgent;
+    const isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+    const isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    const imgData = state.context.canvas.toDataURL('image/png');
+    if (isIOS) {
+      window.webkit.messageHandlers.saveImage.postMessage(imgData);
+    } else if (isAndroid) {
+      window.android.saveImage(imgData);
+    } else {
+      const link = document.createElement('a');
+      link.href = imgData;
+      link.download = 'whiteboard.png';
+      link.click();
+    }
+  }
 }
 
 export default Save;
