7e5979c69a4c41c51d0ec7ad8c6d930da4d0ee27
afei <afei_gf@foxmail.com>
2021-01-11 13:45:17 +0800

    config

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-
+const isDev = process.env.NODE_ENV === 'develop'
 module.exports = {
-  db: 'mongodb://localhost:27017/whiteboard',
+  db: isDev ?
+    'mongodb://localhost:27017/whiteboard' :
+    `mongodb://${process.env['MONGO_USER']}:${process.env['MONGO_PASS']}@${process.env['MONGO_HOST']}:3717`,
   port: 8080,
   // Save after 2 seconds of inactivity
   saveInterval: 1000 * 2,
